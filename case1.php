4f96386b4507f6401b496ba8248167a406d63cf2
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-09 14:54:17 +0100

    Add conditions for wina and fruit tax

--- case1.php
+++ case1.php
@@ -1,17 +1,26 @@
 <?php
+$fruitTaxRate = 0.06;
+$wineTaxRate = 0.21;
+
 
 $basket = [
-    'bananas' => ['pieces' => 6, 'price' => 1],
+    'Bananas' => ['pieces' => 6, 'price' => 1],
     'Apples' => ['pieces' => 3, "price" => 1.5],
-    'Bottles of Wine' => ['pieces' => 2, 'price' => 10]
+    'Wine' => ['pieces' => 2, 'price' => 10]
 ];
 
 foreach ($basket as $item => $deatails) {
     $pieces = $deatails['pieces'];
     $price = $deatails['price'];
+    $itemTotal = $pieces * $price;
+    $TotalPrice += $itemTotal;
+
+    if ($item === 'Bananas' || $item === "Apples") {
+        $fruitTax += $itemTotal = $fruitTaxRate;
+    } elseif ($item === 'Wine') {
+        $wineTax += $itemTotal = $wineTaxRate;
+    }
 }
 
-$fruitTaxRate = 0.06;
-$wineTaxRate = 0.21;
 
 var_dump($basket);
