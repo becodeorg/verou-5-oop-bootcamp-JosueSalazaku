0dbd158a88a173067f96497c79d5a0792ec3dded
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-09 14:24:07 +0100

    Define basket in a array and create a foreach loop

---
+++ case1.php
@@ -0,0 +1,18 @@
+<?php
+
+$basket = [
+    'bananas' => ['pieces' => 6, 'price' => 1],
+    'Apples' => ['pieces' => 3, "price" => 1.5],
+    'Bottles of Wine' => ['pieces' => 2, 'price' => 10]
+];
+
+foreach ($basket as $item => $deatails) {
+    $pieces = $deatails['pieces'];
+    $price = $deatails['price'];
+
+
+}
+
+echo($iteù);
+
+var_dump($basket);
