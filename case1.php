2ca15562028b721ec22bc54e997ea4701f59b94c
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-09 15:17:56 +0100

    Add conditions for wina and fruit tax

--- case1.php
+++ case1.php
@@ -1,26 +1,33 @@
 <?php
 $fruitTaxRate = 0.06;
 $wineTaxRate = 0.21;
-
+$fruitTax = 0;
+$wineTax = 0;
+$totalPrice = 0;
 
 $basket = [
     'Bananas' => ['pieces' => 6, 'price' => 1],
-    'Apples' => ['pieces' => 3, "price" => 1.5],
-    'Wine' => ['pieces' => 2, 'price' => 10]
+    'Apples' => ['pieces' => 3, 'price' => 1.5],
+    'Wine' => ['pieces' => 2, 'price' => 10],
 ];
 
-foreach ($basket as $item => $deatails) {
-    $pieces = $deatails['pieces'];
-    $price = $deatails['price'];
-    $itemTotal = $pieces * $price;
-    $TotalPrice += $itemTotal;
+foreach ($basket as $itemName => $itemDetails) {
+    $quantity = $itemDetails['pieces'];
+    $price = $itemDetails['price'];
+    $totalPrice += $quantity * $price;
 
-    if ($item === 'Bananas' || $item === "Apples") {
-        $fruitTax += $itemTotal = $fruitTaxRate;
-    } elseif ($item === 'Wine') {
-        $wineTax += $itemTotal = $wineTaxRate;
+    if ($itemName === 'Bananas' || $itemName === 'Apples') {
+        $fruitTax += $quantity * $price * $fruitTaxRate;
+    } elseif ($itemName === 'Wine') {
+        $wineTax += $quantity * $price * $wineTaxRate;
     }
 }
 
+$totalTax = $fruitTax + $wineTax;
+$totalPriceWithTax = $totalPrice + $totalTax;
 
-var_dump($basket);
+echo "Total Tax (Fruit): €$fruitTax <br>";
+echo "Total Tax (Wine): €$wineTax <br>";
+echo "Total Price with Tax: €$totalPriceWithTax <br>";
+
+
