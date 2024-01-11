59e637d5cd76107f3c76a6f6a73f02a07e2fc94f
Josué Salazaku <josuesalazaku@gmail.com>
2024-01-11 15:28:50 +0100

    Create a article class with construct funtion and title display function

--- case3/content.php
+++ case3/content.php
@@ -8,7 +8,8 @@ We are preparing three types of content for a website:
 Articles
 Ads
 Vacancies
-All of those have a title and text. When showing the title, they are modified as follows: articles remain as they are, ads are shown in all caps and vacancies are appended with " - apply now!". The original title should still be retrievable, so no modification is permanent.
+All of those have a title and text. When showing the title, they are modified as follows:
+articles remain as they are, ads are shown in all caps and vacancies are appended with " - apply now!". The original title should still be retrievable, so no modification is permanent.
 
 Have an array with two articles, one ad and one vacancy. Use this array to show all content types (title + text).
 
